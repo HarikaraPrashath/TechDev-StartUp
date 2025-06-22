27d515d00b905475a040482cd63583d704f039d6
Harigara Prashath <prashathhari2002@gmail.com>
2025-06-22 19:50:20 +0530

    making icon change

--- my-app/components/Home/About/About.tsx
+++ my-app/components/Home/About/About.tsx
@@ -1,6 +1,7 @@
 import BoxText from '@/components/Helper/BoxText'
 import Image from 'next/image'
 import React from 'react'
+import { BiRightArrowAlt } from 'react-icons/bi'
 
 const About = () => {
   return (
@@ -17,10 +18,12 @@ const About = () => {
                <h1 className='text-2xl sm:text-3xl font-bold text-gray-900 mt-3 leading-[2.5rem] sm:leading-[3rem] '>Everything You Need To Grow Your Business</h1>
 
                <p className='mt-3 leading-relaxed text-sm sm:text-base text-gray-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum cupiditate repellat soluta dolor! Quisquam facere aperiam itaque, optio commodi blanditiis</p>
-               <button className='mt-5 text-[#f68967] text-lg font-bold pd-1 border-b-2 border-[#f68967] '>Learn more &#8594;</button>
+             <button className="mt-5 inline-flex items-center gap-2 text-[#f68967] text-lg font-bold pb-1 border-b-2 border-[#f68967] hover:text-[#f46f4d] transition-all duration-200">
+  Learn more <BiRightArrowAlt className="w-5 h-5" />
+</button>
                <div className='mt-8 border-l-2 border-gray-200'>
                     <div className='ml-6'>
-                        <p className='text-gray-700 font-medium text-lg'>&quot;"The many integrations that can be linked really help me see data form other tools I also use"</p>
+                        <p className='text-gray-700 font-medium text-lg'>"The many integrations that can be linked really help me see data form other tools I also use"</p>
                         <div className='flex items-center space-x-6 mt-6 '>
                             <Image src="/images/u1.jpg" alt='profile' width={40} height={40}
                             className='rounded-full'
